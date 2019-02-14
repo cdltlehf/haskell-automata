c6b7a3fffd8ed501d8fd6a706c01c3b1f8f8a7a1
성시철 <seongsicheol@seongsicheol-ui-MacBook-Pro.local>
2019-02-15 04:57:53 +0900

    Add DFA, NFA, accepts, language

--- src/FA.hs
+++ src/FA.hs
@@ -8,9 +8,9 @@ type Symbols = Set Symbol
 data FA a = DFA (Set a, Symbols, a -> Symbol -> a, a, Set a) |
             NFA (Set a, Symbols, a -> Symbol' -> (Set a), a, Set a)
 
-accept (DFA (_,_,d,q0,f)) w = Set.member (d' q0 w) f 
+accepts (DFA (_,_,d,q0,f)) w = Set.member (d' q0 w) f 
   where d' = foldl d
-accept (NFA (_,_,d,q0,f)) w = any (\e -> Set.member e f) (d' q0 w')
+accepts (NFA (_,_,d,q0,f)) w = any (\e -> Set.member e f) (d' q0 w')
   where
     w' = map Just w
     unionMap f = Set.foldr' (Set.union . f) Set.empty
@@ -23,24 +23,38 @@ accept (NFA (_,_,d,q0,f)) w = any (\e -> Set.member e f) (d' q0 w')
     d' q w = foldl t (e q) w
       where
         t a b = unionMap (\x -> unionMap e $ d x b) a
-        --t = \a b -> (Set.foldr' (Set.union . e) Set.empty) . t'
 
-reject m = not . (accept m)
+reject m = not . (accepts m)
 
-language (DFA m) = filter (accept (DFA m)) s' 
+language (DFA m) = filter (accepts (DFA m)) s' 
   where
     DFA (_,s,_,_,_) = DFA m
     s' = [c:t | t <- "":s', c <- let s' = Set.toList s in s']
-language (NFA m) = filter (accept (NFA m)) s' 
+language (NFA m) = filter (accepts (NFA m)) s' 
   where
     NFA (_,s,_,_,_) = NFA m
     s' = [c:t | t <- "":s', c <- let s' = Set.toList s in s']
 
-d 0 (Just '0') = Set.singleton 0
-d 0 (Just '1') = Set.singleton 1
-d 0 Nothing = Set.empty
-d 1 (Just '0') = Set.singleton 1
-d 1 (Just '1') = Set.singleton 1
-d 1 Nothing = Set.empty
+d 'a' (Just '0') = Set.singleton 'a'
+d 'a' (Just '1') = Set.singleton 'a'
+d 'a' Nothing = Set.fromAscList ['b', 'e']
+d 'b' (Just '0') = Set.singleton 'c'
+d 'b' (Just '1') = Set.empty
+d 'b' Nothing = Set.empty
+d 'c' (Just '0') = Set.empty
+d 'c' (Just '1') = Set.singleton 'd'
+d 'c' Nothing = Set.empty
+d 'd' (Just '0') = Set.empty
+d 'd' (Just '1') = Set.empty
+d 'd' Nothing = Set.empty
+d 'e' (Just '0') = Set.empty
+d 'e' (Just '1') = Set.singleton 'f'
+d 'e' Nothing = Set.empty
+d 'f' (Just '0') = Set.singleton 'g'
+d 'f' (Just '1') = Set.empty
+d 'f' Nothing = Set.empty
+d 'g' (Just '0') = Set.empty
+d 'g' (Just '1') = Set.empty
+d 'g' Nothing = Set.empty
 
-m = NFA (Set.fromAscList [0, 1], Set.fromAscList ['0', '1'], d, 0, Set.fromAscList [1])
+m = NFA (Set.fromAscList ['a'..'g'], Set.fromAscList ['0', '1'], d, 'a', Set.fromAscList ['d','g'])
